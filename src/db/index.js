61cd226e40c8d812b3dd382b1958e1eb05340cba
najiblere <kabir.najib@wayapaychat.com>
2025-01-10 10:08:55 +0100

    content management

--- src/db/index.js
+++ src/db/index.js
@@ -6,14 +6,14 @@ dotenv.config();
 
 // const db = process.env.NODE_ENV ===
 
-// const client = new Client({
-//   connectionString: process.env.DATATYPE === 'test' ? process.env.DATABASE_URL1 : process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
-// });
+const client = new Client({
+  connectionString: process.env.DATATYPE === 'test' ? process.env.DATABASE_URL1 : process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
-const client = new Client(process.env.DATABASE_URL);
+// const client = new Client(process.env.DATABASE_URL);
 
 client.connect();
 
